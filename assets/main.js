67ffd3c947126773dd25f5b6e9be9b470ca77607
Karine Pereira <karinesp89@gmail.com>
2022-11-01 15:07:20 +0100

    add sprite animation and fps control

--- assets/main.js
+++ assets/main.js
@@ -16,8 +16,8 @@ window.addEventListener("load", function () {
 			this.input = new InputHandler();
 		}
 
-		update() {
-			this.player.update(this.input.keys);
+		update(deltaTime) {
+			this.player.update(this.input.keys, deltaTime);
 		}
 
 		draw(context) {
@@ -26,13 +26,19 @@ window.addEventListener("load", function () {
 	}
 
 	const game = new Game(canvas.width, canvas.height);
+	let lastTime = 0;
 
-	function animate() {
+	function animate(timeStamp) {
+		const deltaTime = timeStamp - lastTime;
+		//console.log(deltaTime);
+		lastTime = timeStamp;
+
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		game.update();
+
+		game.update(deltaTime);
 		game.draw(ctx);
 		requestAnimationFrame(animate);
 	}
 
-	animate();
+	animate(0);
 });
--- assets/player.js
+++ assets/player.js
@@ -1,3 +1,5 @@
+import {Sitting, Running, Jumping, Falling} from "./playerStates.js";
+
 export class Player {
 	constructor(game) {
 		this.game = game;
@@ -6,13 +8,24 @@ export class Player {
 		this.x = 0;
 		this.y = this.game.height - this.height;
 		this.image = document.getElementById("player");
+		this.frameX = 0;
+		this.frameY = 0;
+		this.maxFrame = 0;
+		this.fps = 20;
+		this.frameInterval = 1000 / this.fps;
+		this.frameTimer = 0;
 		this.vy = 0;
 		this.weight = 1;
 		this.speed = 0;
-		this.maxSpeed = 5;
+		this.maxSpeed = 10;
+		this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+		this.currentState = this.states[0];
+		this.currentState.enter();
 	}
 
-	update(input) {
+	update(input, deltaTime) {
+		this.currentState.handleInput(input);
+
 		// horizontal moviment
 		this.x += this.speed;
 
@@ -32,21 +45,36 @@ export class Player {
 		}
 
 		//vertical moviment
-		if (input.includes("ArrowUp") && this.onGround()) {
-			this.vy -= 25;
-		}
 		this.y += this.vy;
 		if (!this.onGround()) {
 			this.vy += this.weight;
 		} else this.vy = 0;
+
+		// sprite animation
+		if (this.frameTimer > this.frameInterval) {
+			this.frameTimer = 0;
+			if (this.frameX < this.maxFrame) {
+				this.frameX++;
+				console.log(this.maxFrame);
+			} else {
+				this.frameX = 0;
+			}
+		} else {
+			this.frameTimer += deltaTime;
+		}
 	}
 
 	draw(context) {
-		context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+		context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 	}
 
+	// return true if player is on the gound and false if it's in the air
 	onGround() {
 		return this.y >= this.game.height - this.height;
-		// return true if player is on the gound and false if it's in the air
+	}
+
+	setState(stateIndex) {
+		this.currentState = this.states[stateIndex];
+		this.currentState.enter();
 	}
 }
